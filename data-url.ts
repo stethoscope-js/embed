3e6bf7be7696ec811b1ab7c508e7e64ce5e5b129
Anand Chowdhary <github@anandchowdhary.com>
2026-09-18 21:42:03 +0300

    :sparkles: Show latest adapter run status (#3)

--- data-url.ts
+++ data-url.ts
@@ -5,4 +5,9 @@ export const getDataUrl = (repo: string, api: string, path: string, ref = "maste
     path
   )}`;
 
+export const getManifestUrl = (repo: string, ref = "master") =>
+  `https://raw.githubusercontent.com/${encodePath(repo)}/${encodeURIComponent(
+    ref
+  )}/data/.stethoscope/manifest.v3.json`;
+
 export const getRefQuery = (ref: string) => (ref === "master" ? "" : `&ref=${encodeURIComponent(ref)}`);
